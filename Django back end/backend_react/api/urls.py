c2a5039bbcd8528a348b297d2dade41638dd0efd
alamin <alaminbhuyan322@gmail.com>
2023-05-18 11:06:51 +0600

    add single datashow on form

--- Django back end/backend_react/api/urls.py
+++ Django back end/backend_react/api/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path
 from api import views
+from django.urls import path
 
 app_name = 'api'
 
 urlpatterns = [
     path(route="list/", view=views.PlanList.as_view(), name="listview"),
     path(route='create/', view=views.PlanCreate.as_view(), name='createview'),
-    path(route='delete/<int:id>',view=views.PlanDestroy.as_view(), name='deleteview'),
-    path(route='update/<int:id>',view=views.PlanUpdate.as_view(), name='updateview'),
+    path(route='read/<int:pk>', view=views.PlanRetrieve.as_view(), name='readview'),
+    path(route='update/<int:id>', view=views.PlanUpdate.as_view(), name='updateview'),
+    path(route='delete/<int:id>', view=views.PlanDestroy.as_view(), name='deleteview'),
 ]
--- Django back end/backend_react/api/views.py
+++ Django back end/backend_react/api/views.py
@@ -1,7 +1,8 @@
 from api.models import Plan
 from api.serializers import PlanSerializer
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView, DestroyAPIView, ListAPIView, UpdateAPIView
+from rest_framework.generics import CreateAPIView, DestroyAPIView, ListAPIView, UpdateAPIView, RetrieveAPIView
+
 
 
 # Create your views here.
@@ -14,6 +15,10 @@ class PlanCreate(CreateAPIView):
     queryset = Plan.objects.all()
     serializer_class = PlanSerializer
 
+class PlanRetrieve(RetrieveAPIView):
+    queryset = Plan.objects.all()
+    serializer_class = PlanSerializer
+    lookup_field = 'pk'
 
 class PlanDestroy(DestroyAPIView):
     queryset = Plan.objects.all()
